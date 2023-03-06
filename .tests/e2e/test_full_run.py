77e5b628d0582b0b07054a34e002d9149aaefb1c
ctbus <ctbushman@gmail.com>
2023-03-06 16:07:11 -0500

    Update test data and pointers to it

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -11,7 +11,8 @@ def setup():
     temp_dir = tempfile.mkdtemp()
 
     reads_fp = os.path.abspath(".tests/data/reads/")
-    genomes_fp = os.path.abspath(".tests/data/hosts/")
+    hosts_fp = os.path.abspath(".tests/data/hosts/")
+    genomes_fp = os.path.abspath(".tests/data/ref/")
 
     project_dir = os.path.join(temp_dir, "project/")
 
@@ -20,7 +21,19 @@ def setup():
     config_fp = os.path.join(project_dir, "sunbeam_config.yml")
 
     config_str = f"sbx_mapping: {{genomes_fp: {genomes_fp}}}"
+    sp.check_output(
+        [
+            "sunbeam",
+            "config",
+            "modify",
+            "-i",
+            "-s",
+            f"{config_str}",
+            f"{config_fp}",
+        ]
+    )
 
+    config_str = f"qc: {{host_fp: {hosts_fp}}}"
     sp.check_output(
         [
             "sunbeam",
